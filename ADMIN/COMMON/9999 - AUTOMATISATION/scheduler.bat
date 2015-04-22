0e9d57d234565c926b2218767b1f1266e28b70cd
Philipp Frenzel <philipp@frenzel.net>
2015-04-22 10:07:21 +0200

    moved to r
    instead of l

--- ADMIN/COMMON/9999 - AUTOMATISATION/scheduler.bat
+++ ADMIN/COMMON/9999 - AUTOMATISATION/scheduler.bat
@@ -4,11 +4,12 @@ ECHO %date% %time%: started scheduler.bat >>"scheduler.log"
 REM -------------------------------------------------------
 REM - File: scheduler.bat
 REM - Description: run qv-scripts from the command line
+REM - Options /r run /l loads and executes makros
 REM -------------------------------------------------------
 ECHO Starting reload process
 ECHO ======================================================
 
-"C:\Program Files\QlikView\QV.exe" /l "D:\qlik\ADMIN\PROD\01 Dataloader\01 Reload Task.qvw"
+"C:\Program Files\QlikView\QV.exe" /r "D:\qlik\ADMIN\PROD\01 Dataloader\01 Reload Task.qvw"
 timeout /T 20
 
 ECHO %date% %time%: finished scheduler.bat >>"scheduler.log"
